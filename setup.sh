88ef7f617b265f0683dbc836adeff1c64ce2cd10
Kristian Hesselberg Brox <kristian.brox@gmail.com>
2024-03-16 12:38:02 +0100

    add setup.sh for easy new system configuration

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+# Ensure execution in current dir
+cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+
+set -eux
+
+# List configs to symlink to ~/
+home_dir_configs=( "global_gitignore/gitignore" "zshrc" "zshenv" "bash_profile" "bashrc" "gitconfig")
+
+# Symlink configs for home dir
+for relative_file_path in "${home_dir_configs[@]}"
+do
+  target_dir="${HOME}"
+  current_dir="$(pwd -P)"
+
+  target_file_name="$(basename -- ${relative_file_path})"
+
+  target_file_path="${target_dir}/.${target_file_name}"
+  config_file_path="${current_dir}/${relative_file_path}"
+
+  ln -s "${config_file_path}" "${target_file_path}" || continue
+done
+
+# Symlink config folders to ~/.config
+config_dir_configs=( "karabiner" )
+for relative_path in "${config_dir_configs[@]}"
+do
+  target_dir="${HOME}/.config"
+  current_dir="$(pwd -P)"
+
+  config_path="${current_dir}/${relative_path}"
+
+  ln -s "${config_path}" "${target_dir}" || continue
+done
